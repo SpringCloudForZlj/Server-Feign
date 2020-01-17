155ef60b29b55ee60f33b2cfaa84ebb33dac62ca
15002994538 <15002994538@163.com>
2020-01-17 20:58:48 +0800

    SpringCloud

--- src/main/java/com/xd/feign/service/UserService.java
+++ src/main/java/com/xd/feign/service/UserService.java
@@ -3,6 +3,7 @@ package com.xd.feign.service;
 import com.xd.common.domain.Api;
 import com.xd.feign.hystrix.UserHystrix;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestParam;
  */
 @FeignClient(value = "Master-Admin",fallback = UserHystrix.class)
 public interface UserService {
-    @RequestMapping(value = "GetUserById",method = RequestMethod.GET)
+    @RequestMapping(value = "GetUserById/{userid}",method = RequestMethod.GET)
     //参数里面应该加上@RequestParam这一参数。目的是为了能让它与controller中的参数保持一致
-    public Api GetUserById(@RequestParam(value = "userid") String userid);
+    public Api GetUserById(@PathVariable(value = "userid") String userid);
 }
